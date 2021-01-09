f1c4344ae8d73782aef391e73f4d424366bc7a38
Kemal Fidan <kemalfidan3@gmail.com>
2021-01-09 16:44:46 -0500

    Added test examples

--- jest.config.ts
+++ jest.config.ts
@@ -1,48 +1,18 @@
-import type { Config } from '@jest/types';
+import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  "roots": [
-    "<rootDir>"
-  ],
-  "modulePaths": [
-    "<rootDir>",
-  ],
-  "moduleDirectories": [
-    "node_modules"
-  ],
-  "testMatch": [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)"
-  ],
-  "transform": {
+  preset: "ts-jest",
+  roots: ["<rootDir>"],
+  modulePaths: ["<rootDir>"],
+  moduleDirectories: ["node_modules"],
+  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
-    "\\.j(ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+    "\\.(ts|tsx)$": "babel-jest",
   },
   // Setup Enzyme
-  "snapshotSerializers": ["enzyme-to-json/serializer"],
-  "setupFilesAfterEnv": ["<rootDir>/src/setupEnzyme.ts"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupEnzyme.ts"],
 };
+
 export default config;
-
-
-// module.exports = {
-//   "roots": [
-//     "<rootDir>"
-//   ],
-//   "modulePaths": [
-//     "<rootDir>",
-//   ],
-//   "moduleDirectories": [
-//     "node_modules"
-//   ],
-//   "testMatch": [
-//     "**/__tests__/**/*.+(ts|tsx|js)",
-//     "**/?(*.)+(spec|test).+(ts|tsx|js)"
-//   ],
-//   "transform": {
-//     "^.+\\.(ts|tsx)$": "ts-jest"
-//   },
-//   // Setup Enzyme
-//   "snapshotSerializers": ["enzyme-to-json/serializer"],
-//   "setupFilesAfterEnv": ["<rootDir>/src/setupEnzyme.ts"],
-// }
--- tst/server/User.test.ts
+++ tst/server/User.test.ts
@@ -1,10 +1,10 @@
-  import { getAlteredUsername } from 'server/actions/User';
+import { getAlteredUsername } from "server/actions/User";
 
-  test('valid username', () => {
-    return expect(getAlteredUsername("Kemal Fidan")).resolves.toBe("Kemal Fidan123");
-  });
+test("valid username", () => {
+  return expect(getAlteredUsername("Kemal Fidan")).resolves.toBe("Kemal Fidan123");
+});
 
-  test('invalid username', () => {
-    expect.assertions(1);
-    return expect(getAlteredUsername("")).rejects.toThrowError("Invalid username");
-  });
+test("invalid username", () => {
+  expect.assertions(1);
+  return expect(getAlteredUsername("")).rejects.toThrowError("Invalid username");
+});
