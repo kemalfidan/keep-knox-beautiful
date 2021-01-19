9b247116273c921aeeb4976bcbbfccb0ac26bf14
Kemal Fidan <kemalfidan3@gmail.com>
2021-01-18 23:58:24 -0500

    Undid changes meant for PR

--- server/actions/User.ts
+++ server/actions/User.ts
@@ -1,5 +1,5 @@
 export async function getAlteredUsername(username: string): Promise<string> {
   if (!username) throw new Error("Invalid username");
-  console.log("Adding random logic for PR");
+
   return username + "123";
 }
