baebe2546936afe26c398d53e7af61a6cfd4d8f7
Kemal Fidan <kemalfidan3@gmail.com>
2021-02-10 23:20:52 -0500

    Added volunteer api

---
+++ src/pages/api/volunteer/[volId].ts
@@ -0,0 +1,6 @@
+
+
+// GET /api/volunteer/[id] will return a single volunteer profile that matches the volId
+// UPDATE /api/volunteer/[id] will take new form data and update basic acc info
+
+
---
+++ src/pages/api/volunteer/index.ts
@@ -0,0 +1,6 @@
+
+
+// POST /api/volunteer will create a single volunteer in the system 
+//    ...(may not be needed since we'll auto-create one when they sign up for an event)
+
+
