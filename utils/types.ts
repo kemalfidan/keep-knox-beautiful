b1b97a8f061c5cd22b3786a38b808bb623c27a31
Kemal Fidan <kemalfidan3@gmail.com>
2021-02-11 23:23:22 -0500

    Added contentful type

--- utils/types.ts
+++ utils/types.ts
@@ -4,26 +4,32 @@ import { ObjectID } from "mongodb";
 
 export interface User {
     _id?: ObjectID;
-    email?: string;
-    name?: string;
+    email: string;
+    name: string;
     phone?: string;
     filledForm?: boolean;
     attendedEvents?: Array<Event>;
     signedUpEvents?: Array<Event>;
 }
+
 export interface Event {
     _id?: ObjectID;
-    name?: string;
-    description?: string;
-    date: Date;
+    name: string;
+    description: string;
+    date?: Date;
     start_time: Date;
     end_time: Date;
     location: string;
-    start_registration: Date;
-    end_registration: Date;
+    start_registration?: Date;
+    end_registration?: Date;
     hours: number;
-    image: string;
-    registered_attendees: Array<string>;
-    present_attendees: Array<string>;
-    absent_attendees: Array<string>;
+    image?: ContentfulImage;
+    registered_attendees?: Array<string>;
+    present_attendees?: Array<string>;
+    absent_attendees?: Array<string>;
+}
+
+export interface ContentfulImage {
+    assetID: string;
+    url: string;
 }
