630d833a66664ce29146ad5b182106be7b8864d1
Kemal Fidan <kemalfidan3@gmail.com>
2021-02-14 12:35:41 -0500

    Fixed api descriptions

--- src/pages/api/events/[eventId]/index.ts
+++ src/pages/api/events/[eventId]/index.ts
@@ -3,12 +3,7 @@ import { getEvent } from "server/actions/Event";
 import errors from "utils/errors";
 import { Event } from "utils/types";
 
-// GET /api/events will return a list of (paginated) events
-// POST /api/events will create an event
-
-// @route   GET /api/events
-// @desc    Return a list of paginated events.
-// @access  Public
+// @route   GET /api/event/[eventId] - Returns a single Event object for event eventId. - Public
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         if (!req || !req.query || !req.query.eventId) {
--- src/pages/api/events/index.ts
+++ src/pages/api/events/index.ts
@@ -1,18 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import errors from "utils/errors";
+import { Event } from "utils/types";
 
-// empty for now, check my other branch kemalfidan/create-api-routes for more api routes
-
+// @route   GET /api/events - Return a list of paginated events. - Public
+// @route   POST /api/events - Create an event from form data. - Private
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        res.status(200).json({
-            success: false,
-            payload: {},
-        });
+        if (req.method === "GET") {
+        }
+        else if (req.method === "POST") {
+        }   
     } catch (error) {
         console.error(error instanceof Error && error);
         res.status(400).json({
             success: false,
-            message: {},
+            message: (error instanceof Error && error.message) || errors.GENERIC_ERROR,
         });
     }
 }
