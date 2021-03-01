1ba1023a8d834ef54d6fc446f6191931da9ab8dd
Andrew Rutter <71574118+andlrutt@users.noreply.github.com>
2021-02-28 21:03:37 -0500

    Added text to left and basic form on right

--- src/components/EventForm/index.tsx
+++ src/components/EventForm/index.tsx
@@ -4,27 +4,35 @@ import { makeStyles, createStyles, Theme, withTheme } from "@material-ui/core/st
 import CoreTypography from "src/components/core/typography";
 import colors from "src/components/core/colors";
 import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import constants from "utils/constants";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
             display: "flex",
-            alignItems: "center",
             textAlign: "center",
-            width: "50%",
+            width: "700px",
+            borderStyle: "solid",
+            borderWidth: "2px",
+            borderColor: colors.green,
+            height: "600px",
+            paddingLeft: "0",
         },
 
         leftWrapper: {
             backgroundColor: colors.green,
             color: colors.white,
-            paddingTop: "230px",
-            paddingBottom: "230px",
-            width: "107%",
+            paddingTop: "150px",
+            paddingBottom: "326px",
+            width: "400px",
         },
 
         rightWrapper: {
-            paddingTop: "230px",
-            paddingBottom: "230px",
+            paddingTop: "100px",
+            textAlign: "right",
+            width: "300px",
         },
 
         textWrapper: {
@@ -33,13 +41,22 @@ const useStyles = makeStyles((theme: Theme) =>
 
         form: {
             textAlign: "left",
-            paddingLeft: "30px",
-            fontSmooth: "true",
         },
 
         input: {
             backgroundColor: colors.gray,
             border: "none",
+            height: "40px",
+            fontSize: "20px",
+        },
+
+        column: {
+            paddingLeft: "0px",
+        },
+
+        center: {
+            textAlign: "center",
+            width: "240px",
         },
     })
 );
@@ -76,6 +93,22 @@ export default function EventsForm() {
                             <CoreTypography variant="body1">Phone Number</CoreTypography>
                             <input type="text" name="phoneNumber" className={styles.input} />
                         </label>
+                        <Container className={styles.column}>
+                            <Link>{constants.org.name.short} Volunteer Waiver</Link>
+
+                            <FormControlLabel
+                                control={<Checkbox />}
+                                label={
+                                    <CoreTypography variant="body2" className={styles.center}>
+                                        By checking this box, I have read and acknowledged the waiver.
+                                    </CoreTypography>
+                                }
+                            />
+                        </Container>
+
+                        <Container>
+                            <input type="submit" value="Sign Up" />
+                        </Container>
                     </form>
                 </Container>
             </Container>
