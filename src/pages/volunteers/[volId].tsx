af03dafdf455927424fc24c4d12d986694fe1b69
Ross Ketron <rossketron@gmail.com>
2021-03-18 14:42:59 -0400

    finished reponsiveness and updated styling to match mock-ups

--- src/pages/volunteers/[volId].tsx
+++ src/pages/volunteers/[volId].tsx
@@ -8,7 +8,11 @@ import constants from "utils/constants";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { Paper } from "@material-ui/core";
 import CoreTypography from "src/components/core/typography";
-import { NONAME } from "dns";
+import EmailIcon from "@material-ui/icons/Email";
+import PhoneIcon from "@material-ui/icons/Phone";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import colors from "src/components/core/colors";
 
 interface Props {
     vol: Volunteer;
@@ -20,24 +24,68 @@ const VolunteerPage: NextPage<Props> = ({ vol }) => {
     if (!vol) {
         return <Error statusCode={404} />;
     }
+
+    const firstName = vol.name.split(" ")[0];
+
     return (
         <>
             <div className={classes.container}>
                 <div className={classes.volInfoContainer}>
-                    <Paper className={classes.nameCard} elevation={3}>
-                        <CoreTypography variant="h1">Name</CoreTypography>
+                    <Paper className={classes.nameCard} elevation={2}>
+                        <div className={classes.nameCardContent}>
+                            <CoreTypography variant="body1" style={{ fontSize: "60px" }}>
+                                <div className={classes.nameCardTopRow}>
+                                    {vol.name}
+                                    <div>
+                                        <EditIcon style={{ verticalAlign: "top", marginRight: "5px" }} />
+                                        <DeleteIcon style={{ verticalAlign: "top", marginRight: "5px" }} />
+                                    </div>
+                                </div>
+                            </CoreTypography>
+                            <div>
+                                <CoreTypography variant="body1">
+                                    <EmailIcon fontSize="large" style={{ verticalAlign: "middle" }} />
+                                    &nbsp;&nbsp;{vol.email}
+                                </CoreTypography>
+                                <CoreTypography variant="body1">
+                                    <PhoneIcon fontSize="large" style={{ verticalAlign: "middle" }} />
+                                    &nbsp;&nbsp;{vol.phone}
+                                </CoreTypography>
+                            </div>
+                        </div>
                     </Paper>
                     <div className={classes.volInfoRight}>
-                        <Paper className={classes.rightCards} elevation={3}>
-                            <CoreTypography variant="h1">Name</CoreTypography>
+                        <Paper className={classes.rightCards} elevation={2}>
+                            <div className={classes.rightCardsContent}>
+                                <CoreTypography variant="body1" style={{ fontSize: "130px" }}>
+                                    {/*vol.totalHours*/}6
+                                </CoreTypography>
+                                <CoreTypography variant="body1">Events Attended</CoreTypography>
+                            </div>
                         </Paper>
-                        <Paper className={classes.rightCards} elevation={3}>
-                            <CoreTypography variant="h1">Hours</CoreTypography>
+                        <Paper className={classes.rightCards} elevation={2}>
+                            <div className={classes.rightCardsContent}>
+                                <CoreTypography variant="body1" style={{ fontSize: "130px" }}>
+                                    {/*vol.totalHours*/}
+                                    24
+                                </CoreTypography>
+                                <CoreTypography variant="body1">Total Hours</CoreTypography>
+                            </div>
                         </Paper>
                     </div>
                 </div>
-                <Paper className={classes.nameHeader}>
-                    <CoreTypography variant="h1">Name Event Header</CoreTypography>
+                <Paper className={classes.nameHeader} elevation={0}>
+                    <div className={classes.nameHeaderContent}>
+                        <CoreTypography variant="body1" style={{ fontSize: "35px" }}>
+                            {firstName}&apos;s Events
+                        </CoreTypography>
+                        <button className={classes.hoursVerificationButton}>
+                            <CoreTypography variant="body2">
+                                <EmailIcon fontSize="large" style={{ verticalAlign: "middle" }} />
+                                &nbsp;&nbsp;Total Hours Verification
+                            </CoreTypography>
+                        </button>
+                    </div>
                 </Paper>
                 <VolunteerEventsList {...vol} />
             </div>
@@ -79,13 +127,18 @@ export async function getStaticPaths() {
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             margin: "45px",
         },
         volInfoContainer: {
             display: "flex",
             flexDirection: "row",
             alignItems: "center",
+            marginTop: "40px",
             justifyContent: "space-between",
+            width: "100%",
             [theme.breakpoints.between(0, "sm")]: {
                 flexDirection: "column",
                 width: "100%",
@@ -97,38 +150,85 @@ const useStyles = makeStyles((theme: Theme) =>
             justifyContent: "space-between",
             [theme.breakpoints.between(0, "sm")]: {
                 marginTop: "20px",
-                width: "100%",
+                width: "90%",
             },
         },
         nameCard: {
+            border: `2px solid ${theme.palette.primary.light}`,
             [theme.breakpoints.between(0, "sm")]: {
-                width: "100%",
+                width: "90%",
             },
             "&>*": {
                 margin: theme.spacing(1),
-                width: theme.spacing(70),
+                width: theme.spacing(65),
                 height: theme.spacing(30),
             },
         },
+        nameCardContent: {
+            paddingTop: "10px",
+            paddingLeft: "40px",
+            paddingBottom: "20px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "left",
+            justifyContent: "space-between",
+        },
+        nameCardTopRow: {
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            [theme.breakpoints.between(0, "sm")]: {
+                width: "90%",
+            },
+        },
+        nameCardIcons: {
+            textAlign: "right",
+        },
         rightCards: {
-            marginLeft: "20px",
+            marginLeft: "40px",
+            border: `2px solid ${theme.palette.primary.light}`,
             [theme.breakpoints.between(0, "sm")]: {
                 marginLeft: "0px",
             },
             "&>*": {
                 margin: theme.spacing(1),
-                width: theme.spacing(30),
+                width: theme.spacing(27),
                 height: theme.spacing(30),
             },
         },
+        rightCardsContent: {
+            textAlign: "center",
+        },
         nameHeader: {
-            border: `1px solid ${theme.palette.primary.light}`,
-            marginTop: "20px",
-            marginBottom: "20px",
+            border: `2px solid ${theme.palette.primary.light}`,
+            marginTop: "50px",
+            marginBottom: "30px",
+            width: "100%",
             "&>*": {
                 width: "100%",
                 margin: theme.spacing(1),
-                height: theme.spacing(8),
+                height: theme.spacing(7),
+            },
+            [theme.breakpoints.between(0, "sm")]: {
+                width: "90%",
+            },
+        },
+        nameHeaderContent: {
+            paddingLeft: "5px",
+            paddingRight: "20px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+        },
+        hoursVerificationButton: {
+            color: colors.white,
+            backgroundColor: colors.pink,
+            border: "none",
+            padding: "0 8px",
+            borderRadius: "10px",
+            [theme.breakpoints.between(0, "sm")]: {
+                padding: "0 4px",
             },
         },
     })
