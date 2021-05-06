abdbc8c3114acb48a94003abbf754a4a6e6b6050
Zavier <zavier@resaleai.com>
2021-05-06 12:18:55 -0400

    progression w bypass pre commit

--- server/actions/Event.ts
+++ server/actions/Event.ts
@@ -205,23 +205,6 @@ export const deleteEvent = async function (id: string) {
     }
 };
 
-// helper
-export const getEventWithVolFields = async function (eventId: string) {
-    await mongoDB();
-    if (!eventId || eventId == "") {
-        throw new APIError(400, "Invalid id");
-    }
-
-    const event = await EventSchema.findById(eventId)
-        .populate({ path: "registeredVolunteers" })
-        .populate({ path: "attendedVolunteers" });
-    if (!event) {
-        throw new APIError(404, "Event does not exist");
-    }
-
-    return event;
-};
-
 /**
  * @param eventId The id of the event to get volunteers from.
  * @param page Since this data is paginated, page is used to return a certain subset of the data.
@@ -371,6 +354,7 @@ export const getEventVolunteers = async function (eventId: string, page: number,
             numberRegistered = event?.registeredVolunteers?.length || 0;
             volunteers = event?.registeredVolunteers as Volunteer[];
         } else if (totalRegistered > page * VOLS_PER_PAGE) {
+            console.log(page * VOLS_PER_PAGE, numberAttendedMixed);
             // mixed w/ registered + attended
             // both registered + attended needed, can fetch both in 1 query rather than 2
             const event = await EventSchema.findById(eventId)
